04a80c3e59decc34e19a3db236d2ac67d05116c0
BrunoBitencourt99 <bruno.bitencourt.silva@gmail.com>
2024-11-14 17:11:42 -0300

    Implemento Packed User

---
+++ user/src/main/java/com/caldeira/projetofinal/user/UserApiProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.caldeira.projetofinal.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiProjetoFinalApplication.class, args);
+	}
+
+}
