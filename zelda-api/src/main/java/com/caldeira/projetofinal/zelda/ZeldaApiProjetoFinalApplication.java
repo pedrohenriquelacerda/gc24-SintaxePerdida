44a11be591ae31390fc8bf3567ddf0b1cf0ba15a
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2024-11-18 19:41:03 -0300

    :package: add zelda-api and rename projects

---
+++ zelda-api/src/main/java/com/caldeira/projetofinal/zelda/ZeldaApiProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.caldeira.projetofinal.zelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaApiProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaApiProjetoFinalApplication.class, args);
+	}
+
+}
---
+++ zelda-api/src/test/java/com/caldeira/projetofinal/zelda/ZeldaApiProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caldeira.projetofinal.zelda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeldaApiProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
