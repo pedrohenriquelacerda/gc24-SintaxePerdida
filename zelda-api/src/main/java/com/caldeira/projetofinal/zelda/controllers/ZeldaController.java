f80cf3f5d926408cd8e5eb677015f9771a819665
alunojava-13 <alunojava-13@Eldorados-MacBook-Pro.local>
2024-11-28 11:49:22 -0800

    feat:add-zelda-controller-PF-S03T08

---
+++ zelda-api/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
@@ -0,0 +1,11 @@
+package com.caldeira.projetofinal.zelda.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+@Setter
+@RestController
+public class ZeldaController {
+}
